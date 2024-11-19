a3892ee0f667644eddc917a75d50c263410c9e3a
Chandra4991 <kanthchandra578+a1@gmail.com>
2024-11-19 12:17:03 +0530

    Update K8s.groovy

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -13,7 +13,7 @@ class K8s {
         echo "********************* Entering into Kubernetes Authentication/Login Method *********************"
         gcloud compute instances list
         echo "********************* Get the K8S Node *********************"
-        gcloud container clusters get-credentials i27-cluster --zone us-central1-c --project future-depth-439107-m2
+        gcloud container clusters get-credentials i27-cluster --zone us-central1-c --project fine-climber-412705
         kubectl get nodes
         """
     }
