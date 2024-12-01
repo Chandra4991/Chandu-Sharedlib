b222ec5c096e745198107c9ea43b418f78bda319
Chandra4991 <kanthchandra578+a1@gmail.com>
2024-12-01 17:29:54 +0530

    Update nodePipeline.groovy

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -39,7 +39,7 @@ def call(Map pipelineParams){
 
         environment {
             APPLICATION_NAME = "${pipelineParams.appName}"
-            DOCKER_HUB = "docker.io/i27devopsb4"
+            DOCKER_HUB = "docker.io/devopsa2929"
             DOCKER_CREDS = credentials('dockerhub_creds') //username and password
             K8S_DEV_FILE = "k8s_dev.yaml"
             K8S_TST_FILE = "k8s_tst.yaml"
